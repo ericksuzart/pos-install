8e8a0742f82ab91e3a6238df874b2a7022924e51
Erick Suzart <erick.suzart@ufba.br>
2023-03-29 13:30:08 -0300

    Update popOS_22-04.bash

--- popOS_22-04.bash
+++ popOS_22-04.bash
@@ -172,4 +172,4 @@ sudo apt install boomaga
 # lightweight system monitor
 sudo apt-get install conky
 # conky dependencies
-sudo apt install lm-sensors hddtemp nvme-cli
+sudo apt install lm-sensors hddtemp nvme-cli curl jq
