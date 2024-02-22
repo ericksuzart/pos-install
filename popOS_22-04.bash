1eb4a672aff92b5aeece815fcf8f5ea0ffc6d972
Erick Suzart <erick.suzart@ufba.br>
2024-02-22 07:51:32 -0300

    Update popOS_22-04.bash

--- popOS_22-04.bash
+++ popOS_22-04.bash
@@ -224,3 +224,13 @@ mimic3 'Hello world.' | aplay
 # input remapper: remap input keys
 sudo apt install input-remapper-gtk -y
 sudo systemctl enable --now input-remapper
+
+# install syncthing
+# Add the release PGP keys:
+sudo mkdir -p /etc/apt/keyrings
+sudo curl -L -o /etc/apt/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
+# Add the "stable" channel to your APT sources:
+echo "deb [signed-by=/etc/apt/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+# Update and install syncthing:
+sudo apt-get update
+sudo apt-get install syncthing -y
